622fc105e91cf0931474c92e38faf112baa89890
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-08-26 10:40:42 +0530

    fix: removed unwonted code

--- src/component/Navigation/Navigation.jsx
+++ src/component/Navigation/Navigation.jsx
@@ -5,10 +5,6 @@ const NavBar = () => {
     return(
         <div className="nav-body jstfy-spce-btwn align-centre">
             <h1 className="logo margin-small">Notes <i class="fa fa-book"></i></h1>
-             <div className="login-icon-container jstfy-spce-btwn">
-                <button className="login-Btn margin-small">LOGIN</button>
-                <div className="profile-icon margin-small"><i className="fa fa-user-circle-o"></i></div>
-             </div>
         </div>
     );
 };
